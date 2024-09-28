eb320d3f1fcd8d1643949db0ae9737f01905c695
Caio Costa <hcaio370@gmail.com>
2024-09-28 17:53:57 -0300

    Search places form basic structure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,48 @@
-import FifthHeader from "@/app/components/headings/FifthHeader";
+import Form from "@/app/components/Form"
 
-export default function Home() {
+export default async function Home() {
+  const getCurrentWeatherData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=-23.5475&lon=-46.6361&units=metric&appid=${process.env.API_KEY}`
+      );
+      return response.json();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getFiveDayWeatherForecastData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=-23.5475&lon=-46.6361&units=metric&appid=${process.env.API_KEY}`
+      );
+      return response.json();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getAirPollutiontData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/air_pollution?lat=-23.5475&lon=-46.6361&units=metric&appid=${process.env.API_KEY}`
+      );
+      return response.json();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const currentWeatherData = await getCurrentWeatherData();
+  const fiveDataWeatherForecastData = await getFiveDayWeatherForecastData();
+  const airPollution = await getAirPollutiontData();
+
+  const metrics = ["standart", "metric", "imperial"];
+
   return (
-    <div className="px-20">
-      <FifthHeader title={"Dashboard"} />
+    <div>
+      <Form />
     </div>
   );
 }
